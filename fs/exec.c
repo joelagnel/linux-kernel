1b950cba848f9c66b2bf10b06c297ec06bc65f01
Scott James Remnant <scott@ubuntu.com>
2009-10-27 10:05:32 +0000

    UBUNTU: SAUCE: trace: add trace events for open(), exec() and uselib()
    BugLink: http://bugs.launchpad.net/bugs/462111
    
    This patch uses TRACE_EVENT to add tracepoints for the open(),
    exec() and uselib() syscalls so that ureadahead can cheaply trace
    the boot sequence to determine what to read to speed up the next.
    
    It's not upstream because it will need to be rebased onto the syscall
    trace events whenever that gets merged, and is a stop-gap.
    
    [benzh: 3.14 rebase. Squashed 2 warning fixes. Fixed a new incompatible
    pointer type error for trace_open_exec() due to upstream change]
    
    Change-Id: I3bb5c3f6c2b4b7cfbe7045e342092a4d652bcef2
    Signed-off-by: Scott James Remnant <scott@ubuntu.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Ben Zhang <benzh@chromium.org>
    
    Conflicts:
    	fs/exec.c
    [rebase44(filbranden): Moved the trace_open_exec call to the block where
     fsnotify_open was.]
    Signed-off-by: Filipe Brandenburger <filbranden@chromium.org>
    
    Conflicts:
    	fs/open.c
    	fs/exec.c
    
    [rebase412(groeck): Resolved conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    [rebase510(groeck): Context conflicts (open syscalls rearranged)]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Change-Id: I81ae7cf6bc8e435829666fdf21d13c7fd2c5eba5

--- fs/exec.c
+++ fs/exec.c
@@ -70,6 +70,7 @@
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
 
+#include <trace/events/fs_trace.h>
 #include <trace/events/task.h>
 #include "internal.h"
 
@@ -937,8 +938,11 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 	if (err)
 		goto exit;
 
-	if (name->name[0] != '\0')
+	if (name->name[0] != '\0') {
 		fsnotify_open(file);
+
+		trace_open_exec(name->name);
+	}
 
 out:
 	return file;
