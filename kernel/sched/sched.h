2a4398b55fe258ea53fb1fbc727063298f7eea8f
Joel Fernandes (Google) <joel@joelfernandes.org>
2020-07-27 17:56:14 -0400

    kernel/entry: Add support for core-wide protection of kernel-mode
    Core-scheduling prevents hyperthreads in usermode from attacking each
    other, but it does not do anything about one of the hyperthreads
    entering the kernel for any reason. This leaves the door open for MDS
    and L1TF attacks with concurrent execution sequences between
    hyperthreads.
    
    This patch therefore adds support for protecting all syscall and IRQ
    kernel mode entries. Care is taken to track the outermost usermode exit
    and entry using per-cpu counters. In cases where one of the hyperthreads
    enter the kernel, no additional IPIs are sent. Further, IPIs are avoided
    when not needed - example: idle and non-cookie HTs do not need to be
    forced into kernel mode.
    
    More information about attacks:
    For MDS, it is possible for syscalls, IRQ and softirq handlers to leak
    data to either host or guest attackers. For L1TF, it is possible to leak
    to guest attackers. There is no possible mitigation involving flushing
    of buffers to avoid this since the execution of attacker and victims
    happen concurrently on 2 or more HTs.
    
    Cc: Julien Desfossez <jdesfossez@digitalocean.com>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Cc: Aaron Lu <aaron.lwe@gmail.com>
    Cc: Aubrey Li <aubrey.li@linux.intel.com>
    Cc: Tim Chen <tim.c.chen@intel.com>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Co-developed-by: Vineeth Pillai <viremana@linux.microsoft.com>
    Signed-off-by: Vineeth Pillai <viremana@linux.microsoft.com>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1058,10 +1058,13 @@ struct rq {
 	unsigned int		core_sched_seq;
 	struct rb_root		core_tree;
 	unsigned char		core_forceidle;
+	struct irq_work		core_irq_work; /* To force HT into kernel */
+	unsigned int		core_this_unsafe_nest;
 
 	/* shared state */
 	unsigned int		core_task_seq;
 	unsigned long		core_cookie;
+	unsigned int		core_unsafe_nest;
 #endif
 };
 
