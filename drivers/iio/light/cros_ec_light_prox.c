713210df006394bbf2d7eb4f01e2f2eb0b0267ae
Lukasz Bartosik <ukaszb@google.com>
2021-07-15 18:14:23 +0200

    FIXUP: BACKPORT: FROMLIST: Revert "iio: cros_ec: unify hw fifo attributes into the core file"
    Add fixup.
    
    Signed-off-by: Lukasz Bartosik <ukaszb@google.com>
    Change-Id: If14c70e300c0cb89ad6f47f8da36e683a6cda2e1

--- drivers/iio/light/cros_ec_light_prox.c
+++ drivers/iio/light/cros_ec_light_prox.c
@@ -7,6 +7,7 @@
 
 #include <linux/device.h>
 #include <linux/iio/buffer.h>
+#include <linux/iio/buffer_impl.h>
 #include <linux/iio/common/cros_ec_sensors_core.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/kfifo_buf.h>
@@ -426,7 +427,7 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	iio_buffer_set_attrs(indio_dev->buffer, cros_ec_sensor_fifo_attributes);
+	indio_dev->buffer->attrs = cros_ec_sensor_fifo_attributes;
 
 	indio_dev->info = &cros_ec_light_prox_info;
 	state = iio_priv(indio_dev);
