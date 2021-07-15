21b2a66fb4d666ac1f2277f3479c8a506cd80af8
Lukasz Bartosik <ukaszb@google.com>
2021-07-15 18:14:23 +0200

    FIXUP: BACKPORT: FROMLIST: Revert "iio: cros_ec: unify hw fifo attributes into the core file"
    Add fixup.
    
    Signed-off-by: Lukasz Bartosik <ukaszb@google.com>
    Change-Id: If14c70e300c0cb89ad6f47f8da36e683a6cda2e1

--- drivers/iio/common/cros_ec_sensors/cros_ec_sensors.c
+++ drivers/iio/common/cros_ec_sensors/cros_ec_sensors.c
@@ -11,6 +11,7 @@
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/iio/buffer.h>
+#include <linux/iio/buffer_impl.h>
 #include <linux/iio/common/cros_ec_sensors_core.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/kfifo_buf.h>
@@ -252,7 +253,7 @@ static int cros_ec_sensors_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	iio_buffer_set_attrs(indio_dev->buffer, cros_ec_sensor_fifo_attributes);
+	indio_dev->buffer->attrs = cros_ec_sensor_fifo_attributes;
 
 	indio_dev->info = &ec_sensors_info;
 	state = iio_priv(indio_dev);
