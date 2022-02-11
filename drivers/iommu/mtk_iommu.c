62c1680a37f7dbf74da844af33542db63de75d42
Yong Wu <yong.wu@mediatek.com>
2022-02-11 15:08:27 +0800

    Revert "FROMLIST: memory: mtk-smi: Get rid of mtk_smi_larb_get/put"
    Revert "FROMLIST: dt-binding: mediatek: Get rid of mediatek, larb for multimedia HW"
    This reverts commit 81c356de527cd4073ff140ca39efd0ea55427ba9.
    Revert "FROMLIST: memory: mtk-smi: Get rid of mtk_smi_larb_get/put"
    This reverts commit e0b2c5727626a39aa86c9f68b1d52262d6309300.
    Revert "BACKPORT: FROMLIST: drm/mediatek: Get rid of mtk_smi_larb_get/put"
    This reverts commit 45d9a4a3451963722f9f63d1247a2b74cd267939.
    Revert "FROMLIST: drm/mediatek: Add pm runtime support for ovl and rdma"
    This reverts commit b2528a7abf57d773a410c7cfdf9c36d206b1d042.
    Revert "FROMLIST: media: mtk-vcodec: Get rid of mtk_smi_larb_get/put"
    This reverts commit d27e2aeca797d1dd2790d0e15a2ef5ea4309bba4.
    Revert "FROMLIST: media: mtk-mdp: Get rid of mtk_smi_larb_get/put"
    This reverts commit eb4da80f6522f2d5b8cf2be5466363e990b31417.
    Revert "FROMLIST: media: mtk-jpeg: Get rid of mtk_smi_larb_get/put"
    This reverts commit 52e67eea10852cfc46997271704b946cd4774b00.
    Revert "FROMLIST: iommu/mediatek: Add device_link between the consumer and the larb devices"
    This reverts commit 7df81352b02e3aa935823dd3068b562d38a059a4.
    Revert "FROMLIST: iommu/mediatek: Add probe_defer for smi-larb"
    This reverts commit bcffc3608bf534e4ff1f1d07cb95c8857d33f86f.
    
    BUG=b:126008328
    TEST=emerge-corsola sys-kernel/chromeos-kernel-5_15
    
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Change-Id: Icfa63e5d20cd04bf67f8c6e47769479fde6e3f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3454898
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>
    Tested-by: Hsin-Yi Wang <hsinyi@chromium.org>

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -772,9 +772,6 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 {
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
 	struct mtk_iommu_data *data;
-	struct device_link *link;
-	struct device *larbdev;
-	unsigned int larbid, larbidx, i;
 
 	if (!fwspec || fwspec->ops != &mtk_iommu_ops)
 		return ERR_PTR(-ENODEV); /* Not a iommu client device */
@@ -784,50 +781,15 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 	if (!MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_MM))
 		return &data->iommu;
 
-	/*
-	 * Link the consumer device with the smi-larb device(supplier).
-	 * The device that connects with each a larb is a independent HW.
-	 * All the ports in each a device should be in the same larbs.
-	 */
-	larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
-	if (larbid >= MTK_LARB_NR_MAX)
-		return ERR_PTR(-EINVAL);
-
-	for (i = 1; i < fwspec->num_ids; i++) {
-		larbidx = MTK_M4U_TO_LARB(fwspec->ids[i]);
-		if (larbid != larbidx) {
-			dev_err(dev, "Can only use one larb. Fail@larb%d-%d.\n",
-				larbid, larbidx);
-			return ERR_PTR(-EINVAL);
-		}
-	}
-	larbdev = data->larb_imu[larbid].dev;
-	if (!larbdev)
-		return ERR_PTR(-EINVAL);
-
-	link = device_link_add(dev, larbdev,
-			       DL_FLAG_PM_RUNTIME | DL_FLAG_STATELESS);
-	if (!link)
-		dev_err(dev, "Unable to link %s\n", dev_name(larbdev));
 	return &data->iommu;
 }
 
 static void mtk_iommu_release_device(struct device *dev)
 {
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
-	struct mtk_iommu_data *data;
-	struct device *larbdev;
-	unsigned int larbid;
 
 	if (!fwspec || fwspec->ops != &mtk_iommu_ops)
 		return;
-
-	data = dev_iommu_priv_get(dev);
-	if (MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_MM)) {
-		larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
-		larbdev = data->larb_imu[larbid].dev;
-		device_link_remove(dev, larbdev);
-	}
 
 	iommu_fwspec_free(dev);
 }
