b4ae6e4020be575a7f6e3bdcb2b534be07560f20
Valentin Schneider <valentin.schneider@arm.com>
2022-10-03 21:44:59 +0000

    sched/rt: Fix proxy/current (push,pull)ability
    Proxy execution forms atomic pairs of tasks: a proxy (scheduling context)
    and an owner (execution context). The proxy, along with the rest of the
    blocked chain, follows the owner wrt CPU placement.
    
    They can be the same task, in which case push/pull doesn't need any
    modification. When they are different, however,
    FIFO1 & FIFO42:
    
    	     ,->  RT42
    	     |     | blocked-on
    	     |     v
      proxied-by |   mutex
    	     |     | owner
    	     |     v
    	     `--  RT1
    
       RT1
       RT42
    
      CPU0            CPU1
       ^                ^
       |                |
      overloaded    !overloaded
      rq prio = 42  rq prio = 0
    
    RT1 is eligible to be pushed to CPU1, but should that happen it will
    "carry" RT42 along. Clearly here neither RT1 nor RT42 must be seen as
    push/pullable.
    
    Furthermore, tasks becoming blocked on a mutex don't need an explicit
    dequeue/enqueue cycle to be made (push/pull)able: they have to be running
    to block on a mutex, thus they will eventually hit put_prev_task().
    
    XXX: pinned tasks becoming unblocked should be removed from the push/pull
    lists, but those don't get to see __schedule() straight away.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6891,12 +6891,28 @@ blocked_task:
 
 	return NULL; /* retry task selection */
 }
+
+static inline void proxy_tag_curr(struct rq *rq, struct task_struct *next)
+{
+	/*
+	 * pick_next_task() calls set_next_task() on the proxy at some
+	 * point, which ensures it is not push/pullable. However, the
+	 * proxy *and* the owner form an atomic pair wrt push/pull.
+	 *
+	 * Make sure owner is not pushable. Unfortunately we can only
+	 * deal with that by means of a dequeue/enqueue cycle. :-/
+	 */
+	dequeue_task(rq, next, DEQUEUE_NOCLOCK | DEQUEUE_SAVE);
+	enqueue_task(rq, next, ENQUEUE_NOCLOCK | ENQUEUE_RESTORE);
+}
 #else /* PROXY_EXEC */
 static struct task_struct *
 proxy(struct rq *rq, struct task_struct *next, struct rq_flags *rf)
 {
 	return next;
 }
+
+static inline void proxy_tag_curr(struct rq *rq, struct task_struct *next) { }
 #endif /* PROXY_EXEC */
 
 /*
@@ -6945,6 +6961,7 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	unsigned long prev_state;
 	struct rq_flags rf;
 	struct rq *rq;
+	bool proxied;
 	int cpu;
 
 	cpu = smp_processor_id();
@@ -7017,20 +7034,11 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 				atomic_inc(&rq->nr_iowait);
 				delayacct_blkio_start();
 			}
-		} else {
-			/*
-			 * XXX
-			 * Let's make this task, which is blocked on
-			 * a mutex, (push/pull)able (RT/DL).
-			 * Unfortunately we can only deal with that by
-			 * means of a dequeue/enqueue cycle. :-/
-			 */
-			dequeue_task(rq, prev, 0);
-			enqueue_task(rq, prev, 0);
 		}
 		switch_count = &prev->nvcsw;
 	}
 
+	proxied = !!prev->blocked_proxy;
 pick_again:
 	/*
 	 * If picked task is actually blocked it means that it can act as a
@@ -7061,6 +7069,10 @@ pick_again:
 		 * changes to task_struct made by pick_next_task().
 		 */
 		RCU_INIT_POINTER(rq->curr, next);
+
+		if (unlikely(!task_current_proxy(rq, next)))
+			proxy_tag_curr(rq, next);
+
 		/*
 		 * The membarrier system call requires each architecture
 		 * to have a full memory barrier after updating
@@ -7085,6 +7097,10 @@ pick_again:
 		/* Also unlocks the rq: */
 		rq = context_switch(rq, prev, next, &rf);
 	} else {
+		/* In case next was already curr but just got blocked_proxy */
+		if (unlikely(!proxied && next->blocked_proxy))
+			proxy_tag_curr(rq, next);
+
 		rq->clock_update_flags &= ~(RQCF_ACT_SKIP|RQCF_REQ_SKIP);
 
 		rq_unpin_lock(rq, &rf);
